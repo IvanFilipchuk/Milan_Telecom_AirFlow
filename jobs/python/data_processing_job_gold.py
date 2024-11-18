5d2cc173ea4d1249fc5dc01c021515c18180923b
YuriiMaisuradze <maisooradze@gmail.com>
2024-11-18 20:58:31 +0100

    Minor fixes

--- jobs/python/data_processing_job_gold.py
+++ jobs/python/data_processing_job_gold.py
@@ -1,38 +1,26 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import sum, avg
-
-# Inicjalizacja SparkSession
 spark = SparkSession.builder \
     .appName("Gold Layer Aggregation") \
     .getOrCreate()
-
-# Ścieżki do danych silver
 input_path_internet = "/opt/data/silver/internet"
 input_path_sms_call = "/opt/data/silver/sms_call"
 output_path_gold_grid = "/opt/data/gold/aggregated_by_gridid"
 output_path_gold_country = "/opt/data/gold/aggregated_by_country"
-
-# Wczytanie danych silver
 internet_data = spark.read.csv(input_path_internet, header=True, inferSchema=True)
 sms_call_data = spark.read.csv(input_path_sms_call, header=True, inferSchema=True)
 
 # --- Agregacja po GridID ---
-
-# Agregacja danych internetowych według GridID
 aggregated_internet_grid = internet_data.groupBy("GridID") \
     .agg(
         sum("transfer").alias("total_internet_transfer"),
         avg("transfer").alias("average_internet_transfer")
     )
-
-# Zapis danych internetowych zagregowanych według GridID
 aggregated_internet_grid.repartition(1) \
     .write \
     .mode("overwrite") \
     .option("header", "true") \
     .csv(f"{output_path_gold_grid}/internet")
-
-# Agregacja danych SMS/Call według GridID
 aggregated_sms_call_grid = sms_call_data.groupBy("GridID") \
     .agg(
         sum("sms_count").alias("total_sms_count"),
@@ -40,8 +28,6 @@ aggregated_sms_call_grid = sms_call_data.groupBy("GridID") \
         sum("call_time").alias("total_call_time"),
         avg("call_time").alias("average_call_time")
     )
-
-# Zapis danych SMS/Call zagregowanych według GridID
 aggregated_sms_call_grid.repartition(1) \
     .write \
     .mode("overwrite") \
@@ -50,21 +36,16 @@ aggregated_sms_call_grid.repartition(1) \
 
 # --- Agregacja po countrycode ---
 
-# Agregacja danych internetowych według countrycode
 aggregated_internet_country = internet_data.groupBy("countrycode") \
     .agg(
         sum("transfer").alias("total_internet_transfer"),
         avg("transfer").alias("average_internet_transfer")
     )
-
-# Zapis danych internetowych zagregowanych według countrycode
 aggregated_internet_country.repartition(1) \
     .write \
     .mode("overwrite") \
     .option("header", "true") \
     .csv(f"{output_path_gold_country}/internet")
-
-# Agregacja danych SMS/Call według countrycode
 aggregated_sms_call_country = sms_call_data.groupBy("countrycode") \
     .agg(
         sum("sms_count").alias("total_sms_count"),
@@ -72,8 +53,6 @@ aggregated_sms_call_country = sms_call_data.groupBy("countrycode") \
         sum("call_time").alias("total_call_time"),
         avg("call_time").alias("average_call_time")
     )
-
-# Zapis danych SMS/Call zagregowanych według countrycode
 aggregated_sms_call_country.repartition(1) \
     .write \
     .mode("overwrite") \
