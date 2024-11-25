fa150f31b38520a0af24a55c653d577ed27efc57
Ivan Filipchuk <ivanflpck@gmial.com>
2024-11-25 21:23:00 +0100

    Modified schema definitions.

--- jobs/python/gold_layer/aggregate_by_country.py
+++ jobs/python/gold_layer/aggregate_by_country.py
@@ -7,25 +7,27 @@ spark = SparkSession.builder \
     .appName("Gold Layer Aggregation by CountryCode") \
     .getOrCreate()
 
-internet_schema = StructType([
-    StructField("countrycode", IntegerType(), True),
-    StructField("transfer", DoubleType(), True)
-])
-
-sms_call_schema = StructType([
-    StructField("countrycode", IntegerType(), True),
-    StructField("sms_count", DoubleType(), True),
-    StructField("call_time", DoubleType(), True)
-])
+# internet_schema = StructType([
+#     StructField("countrycode", IntegerType(), True),
+#     StructField("transfer", DoubleType(), True)
+# ])
+#
+# sms_call_schema = StructType([
+#     StructField("countrycode", IntegerType(), True),
+#     StructField("sms_count", DoubleType(), True),
+#     StructField("call_time", DoubleType(), True)
+# ])
 
 input_path_internet = sys.argv[1] + "/internet"
 input_path_sms_call = sys.argv[1] + "/sms_call"
 output_path_gold_country = sys.argv[2]
 partition_number = int(sys.argv[3])
 
+# internet_data = spark.read.csv(input_path_internet, header=True, schema=internet_schema)
+# sms_call_data = spark.read.csv(input_path_sms_call, header=True, schema=sms_call_schema)
 
-internet_data = spark.read.csv(input_path_internet, header=True, schema=internet_schema)
-sms_call_data = spark.read.csv(input_path_sms_call, header=True, schema=sms_call_schema)
+internet_data = spark.read.csv(input_path_internet, header=True, inferSchema=True)
+sms_call_data = spark.read.csv(input_path_sms_call, header=True, inferSchema=True)
 
 aggregated_internet_country = internet_data.groupBy("countrycode") \
     .agg(
--- jobs/python/gold_layer/aggregate_by_gridid.py
+++ jobs/python/gold_layer/aggregate_by_gridid.py
@@ -7,16 +7,16 @@ spark = SparkSession.builder \
     .appName("Gold Layer Aggregation by GridID") \
     .getOrCreate()
 
-internet_schema = StructType([
-    StructField("GridID", IntegerType(), True),
-    StructField("transfer", DoubleType(), True)
-])
-
-sms_call_schema = StructType([
-    StructField("GridID", IntegerType(), True),
-    StructField("sms_count", DoubleType(), True),
-    StructField("call_time", DoubleType(), True)
-])
+# internet_schema = StructType([
+#     StructField("GridID", IntegerType(), True),
+#     StructField("transfer", DoubleType(), True)
+# ])
+#
+# sms_call_schema = StructType([
+#     StructField("GridID", IntegerType(), True),
+#     StructField("sms_count", DoubleType(), True),
+#     StructField("call_time", DoubleType(), True)
+# ])
 
 input_path_internet = sys.argv[1] + "/internet"
 input_path_sms_call = sys.argv[1] + "/sms_call"
@@ -24,8 +24,11 @@ output_path_gold_grid = sys.argv[2]
 partition_number = int(sys.argv[3])
 
 
-internet_data = spark.read.csv(input_path_internet, header=True, schema=internet_schema)
-sms_call_data = spark.read.csv(input_path_sms_call, header=True, schema=sms_call_schema)
+# internet_data = spark.read.csv(input_path_internet, header=True, schema=internet_schema)
+# sms_call_data = spark.read.csv(input_path_sms_call, header=True, schema=sms_call_schema)
+
+internet_data = spark.read.csv(input_path_internet, header=True, inferSchema=True)
+sms_call_data = spark.read.csv(input_path_sms_call, header=True, inferSchema=True)
 
 aggregated_internet_grid = internet_data.groupBy("GridID") \
     .agg(
